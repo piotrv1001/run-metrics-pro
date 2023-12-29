5f3d8f0c904de2c2c46e3f15086cdc3977c725e0
piotrv1001 <piotrv1001@interia.pl>
2023-12-29 20:09:11 +0100

    Custom toast variants for success and error

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -30,6 +30,8 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success: "border border-green-400",
+        error: "border border-red-400",
       },
     },
     defaultVariants: {
