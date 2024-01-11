e9abd3ace296adcc66830f955dbbc4360743f532
piotrv1001 <piotrv1001@interia.pl>
2024-01-11 14:30:04 +0100

    Spent hours on this smh

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,13 +6,15 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  <div className="h-full relative">
-    <aside className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[40] border-r">
-      <Sidebar />
-    </aside>
-    <main className="md:pl-[304px] px-4 max-w-screen-2xl mx-auto pb-4">
-      <Navbar />
-      {children}
-    </main>
-  </div>;
+  return (
+    <div className="h-full relative">
+      <aside className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[40] border-r">
+        <Sidebar />
+      </aside>
+      <main className="md:pl-[304px] px-4 max-w-screen-2xl mx-auto pb-4">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
 }
