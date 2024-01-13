499c0ead97f7fdc10005d0281acf8855f628a17f
piotrv1001 <piotrv1001@interia.pl>
2024-01-13 18:43:18 +0100

    Updated package json with prisma postinstall

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
 import ThemeToggle from "./theme-toggle";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
+  const { userId } = auth();
   return (
     <div className="flex items-center py-4">
       <MobileSidebar />
-      <div className="flex w-full justify-end gap-x-4 items-center">
+      <div
+        className={cn("flex w-full justify-end items-center", {
+          "gap-x-4": userId,
+        })}
+      >
         <ThemeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
